dd4d8a78a5a7dcc610c8ff9b6d36706305879c56
TonyLo123 <tonylo19961128@gmail.com>
2020-07-09 18:02:13 +0800

    first design of kmbLogo finish

--- lib/KmbCard.dart
+++ lib/KmbCard.dart
@@ -4,16 +4,42 @@ class KmbCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: 150,
-        height: 150,
-        child: Card(
+      width: 350,
+      height: 350,
+      child: Card(
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(75),
+            borderRadius: BorderRadius.circular(175),
           ),
           color: Colors.red,
-          child: Center(
-            child: Text("Mother Fucker !"),
-          ),
-        ));
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 150,
+                height: 100,
+                child: Image.network(
+                    "https://t6.rbxcdn.com/41f3702acfff23d966b3f4d8a1121203"),
+              ),
+              SizedBox(
+                width: 300,
+                height: 100,
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    color: Colors.white,
+                  ),
+                  child: Text("Mother Fukcer ! ! !"),
+                ),
+              ),
+            ],
+          )),
+    );
   }
 }
+
+// Align(
+//           alignment: Alignment.center,
+//           child: Text("My Text"),
+//         ),
